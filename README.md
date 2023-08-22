1a44aa8e0ae855bfcd30d4bdf85a319696d9706a
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-08-22 14:08:17 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CBDateRangePicker
+
+A description of this package.
---
+++ Sources/CBDateRangePicker/CBDateRangePicker.swift
@@ -0,0 +1,6 @@
+public struct CBDateRangePicker {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
